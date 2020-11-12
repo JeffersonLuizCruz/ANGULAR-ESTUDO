6a3b74dcc17bfb080b15a44bace10ea37426f8d1
Jefferson Luiz Cruz <jefferson.luiz.cruz@gmail.com>
2020-11-12 15:22:36 -0300

    Auto stash before merge of "master" and "origin/master"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,20 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/template/header/header.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
+<<<<<<< Updated upstream
 import { MatCardModule } from '@angular/material/card'
+=======
+import { MatCardModule } from '@angular/material/card';
+>>>>>>> Stashed changes
 import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { HomeComponent } from './view/home/home.component';
+<<<<<<< Updated upstream
 import { ProductCrudComponent } from './view/product-crud/product-crud.component';
+=======
+>>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
@@ -20,8 +27,12 @@ import { ProductCrudComponent } from './view/product-crud/product-crud.component
     HeaderComponent,
     FooterComponent,
     NavComponent,
+<<<<<<< Updated upstream
     HomeComponent,
     ProductCrudComponent
+=======
+    HomeComponent
+>>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
