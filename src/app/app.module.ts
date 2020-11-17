6ad6e619114161e48072d6abf6dcd12442246c86
Jefferson Luiz Cruz <jefferson.luiz.cruz@gmail.com>
2020-11-16 22:54:30 -0300

    Fineshed incompleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,7 +23,6 @@ import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { ProductReadComponent } from './components/product/product-read/product-read.component';
-import { ProductRead2Component } from './components/product/product-read2/product-read2.component';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
@@ -39,8 +38,7 @@ import { MatSortModule } from '@angular/material/sort';
     RedDirective,
     MyForDirective,
     ProductCreateComponent,
-    ProductReadComponent,
-    ProductRead2Component
+    ProductReadComponent
   ],
   imports: [
     BrowserModule,
